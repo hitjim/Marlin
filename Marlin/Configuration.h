762844356fbc4f4053be8f58e0f7d170827ba3af
Jim Rice <hitjim@gmail.com>
2020-08-23 22:15:16 -0500

    changing back to measured DELTA_RADIUS and designed printable radius

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -656,7 +656,7 @@
   #endif
 
   // Print surface diameter/2 minus unreachable space (avoid collisions with vertical towers).
-  #define DELTA_PRINTABLE_RADIUS 80.0    // (mm)
+  #define DELTA_PRINTABLE_RADIUS 100.0    // (mm)
 
   // Center-to-center distance of the holes in the diagonal push rods.
   #define DELTA_DIAGONAL_ROD 288.0        // (mm)
@@ -667,8 +667,8 @@
   #define DELTA_ENDSTOP_ADJ { -0.00, -0.36, -1.48 } // Get these values from G33 auto calibrate
 
   // Horizontal distance bridged by diagonal push rods when effector is centered.
-  //#define DELTA_RADIUS 181.00 // measured
-  #define DELTA_RADIUS 178.97              // (mm) Get this value from G33 auto calibrate
+  #define DELTA_RADIUS 181.00 // measured
+  //#define DELTA_RADIUS 178.97              // (mm) Get this value from G33 auto calibrate
 
   // Trim adjustments for individual towers
   // tower angle corrections for X and Y tower / rotate XYZ so Z tower angle = 0
